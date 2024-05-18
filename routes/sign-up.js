2d2ac1642892c3e8aecdf7d3ddee588dbaa95ea5
High23 <warenasb2@gmail.com>
2024-05-17 21:10:35 -0700

    Created get and post methods for sign up

---
+++ routes/sign-up.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const router = express.Router()
+const User = require("../models/user")
+const {body, validationResult} = require("express-validator");
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs")
+
+router.get("/", (req, res) => res.render("sign-up-form"));
+
+router.post("/",[
+    body("firstName")
+    .trim()
+    .isLength({min: 1, max: 50})
+    .escape(),
+    body("lastName")
+    .trim()
+    .isLength({min: 1, max: 50})
+    .escape(),
+    body("email")
+    .trim()
+    .isLength({min: 1, max: 50})
+    .escape(),
+    
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const user = new User({
+            username: req.body.username,
+            password: bcrypt.hash(req.body.password, 10)
+        });
+        if (!errors.isEmpty()) {
+            res.render("sign-up-form", {
+                user: user,
+                errors: errors.array(),
+            });
+        };
+        await user.save();
+        res.redirect("/login");
+    })
+]);
+
+module.exports = router;
