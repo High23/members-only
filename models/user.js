7ca1f52c7a6e0d17bd092cf69b9a3aa6299e91db
High23 <warenasb2@gmail.com>
2024-05-17 21:16:06 -0700

    Removed max length on password

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
     firstName: {type: String, minLength: 1, maxLength: 50, required: true},
     lastName: {type: String, minLength: 1, maxLength: 50, required: true},
-    username: {type: String, minLength: 1, maxLength: 100, required: true},
-    password: {type: String, minLength: 7, maxLength: 20, required: true},
+    username: {type: String, minLength: 3, maxLength: 100, required: true},
+    password: {type: String, minLength: 7, required: true},
     membershipStatus: {type: Boolean},
 });
 
