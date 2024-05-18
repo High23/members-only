27c1078bb3ae5c7833050e77cc12474459d74696
High23 <warenasb2@gmail.com>
2024-05-17 19:48:52 -0700

    Added passport local strat and sign up router

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
+const passport = require("passport");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const signUpRouter = require('./routes/sign-up');
 
-require('dotenv').config()
+const User = require("./models/user");
+
+require('dotenv').config();
 
 const app = express();
 
@@ -24,14 +30,50 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(session({secret: process.env.SECRET, resave: false, saveUninitialized: true}))
+app.use(passport.session())
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      };
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Incorrect password" })
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    };
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/sign-up', signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
