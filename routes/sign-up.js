a0a7ff27a7d18dd198f61531e2a57a9fa9b83d5e
High23 <warenasb2@gmail.com>
2024-05-21 21:25:41 -0700

    Added more form validation

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const User = require("../models/user")
 const {body, validationResult} = require("express-validator");
 const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
 router.get("/", (req, res) => res.render("sign-up-form"));
 
@@ -16,24 +16,49 @@ router.post("/",[
     .trim()
     .isLength({min: 1, max: 50})
     .escape(),
-    body("email")
+    body("email", "The email must be a valid email (gmail, outlook, protonmail, etc.).")
     .trim()
     .isLength({min: 1, max: 50})
+    .isEmail()
+    .custom(async value => {
+        const user = await UserCollection.findUserByEmail(value);
+        if (user) {
+          throw new Error('E-mail already in use');
+        }
+    })
     .escape(),
+    body("password", "The password needs a uppercase(A), lowercase(A) character as well as a number(1) and a symbol(#).").isStrongPassword({
+        minLength: 7,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+        returnScore: false,
+        pointsPerUnique: 1,
+        pointsPerRepeat: 0.5,
+        pointsForContainingLower: 10,
+        pointsForContainingUpper: 10,
+        pointsForContainingNumber: 10,
+        pointsForContainingSymbol: 10,
+    }),
+    body("confirmPassword", "The passwords NEED to match.").custom((value, {req}) => {
+        return value === req.body.password;
+    }),
     
     asyncHandler(async (req, res, next) => {
-        const errors = validationResult(req);
         const user = new User({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
-            username: req.body.email,
-            password: await bcrypt.hash(req.body.password, 10)
+            email: req.body.email,
+            password: await bcrypt.hash(req.body.password, 10),
+            membershipStatus: false
         });
         if (!errors.isEmpty()) {
             res.render("sign-up-form", {
                 user: user,
                 errors: errors.array(),
             });
+            return
         };
         await user.save();
         res.redirect("/login");
