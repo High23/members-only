d37623422c83542a1885a09507ecab05b6eb52df
High23 <warenasb2@gmail.com>
2024-05-21 21:26:14 -0700

    Renamed 'username' to 'email'

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
     firstName: {type: String, minLength: 1, maxLength: 50, required: true},
     lastName: {type: String, minLength: 1, maxLength: 50, required: true},
-    username: {type: String, minLength: 3, maxLength: 100, required: true},
+    email: {type: String, minLength: 3, maxLength: 100, required: true},
     password: {type: String, minLength: 7, required: true},
     membershipStatus: {type: Boolean},
 });
