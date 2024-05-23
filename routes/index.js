efbd8bdc56a764a2d2d316c6a7789bc397c66305
High23 <warenasb2@gmail.com>
2024-05-22 22:23:40 -0700

    Added logout route methods

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.render('index', { title: 'Express', authenticated: true });
+  }
   res.render('index', { title: 'Express' });
+});
+
+router.get('/logout', function(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.render('logout', { authenticated: true });
+  }
+});
+
+router.post('/logout', function(req, res, next) {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+  
 });
 
 module.exports = router;
