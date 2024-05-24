8dd523d53cd09bfdd847761c574c3e1c27506da2
High23 <warenasb2@gmail.com>
2024-05-23 23:23:29 -0700

    Adjusted message schema

--- models/message.js
+++ models/message.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
 const MessageSchema = new Schema({
-    title: {type: String, minLength: 3, maxLength: 100, required: true},
+    title: {type: String, minLength: 3, maxLength: 75, required: true},
     timestamp: {type: String, required: true},
-    text: {type: String, minLength: 3, maxLength: 250, required: true}
+    text: {type: String, minLength: 3, maxLength: 250, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'User', default: undefined},
 });
 
 module.exports = mongoose.model("Message", MessageSchema)
