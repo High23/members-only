48f556a43592d2d588ba6d5d20277a688fc828ca
High23 <warenasb2@gmail.com>
2024-05-23 23:24:01 -0700

    Created get/post routes for message creation

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const { isAuth } = require('../public/javascripts/isAuth');
-const { body } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const User = require('../models/user');
 const Message = require('../models/message');
 const expressAsyncHandler = require('express-async-handler');
 const router = express.Router();
+const { format } = require('date-fns')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const messages = Message.find({}).exec();
+router.get('/', expressAsyncHandler(async function(req, res, next) {
+  const messages = await Message.find({}).populate("author").exec();
 
   if (req.isAuthenticated()) {
     return res.render('index', { 
@@ -19,7 +20,7 @@ router.get('/', function(req, res, next) {
   };
 
   res.render('index', { messages: messages });
-});
+}));
 
 router.get('/logout', isAuth, function(req, res, next) {
   return res.render('logout', { authenticated: true });
@@ -49,9 +50,45 @@ router.post('/join-members', isAuth, body('membersCode').trim(), expressAsyncHan
       membershipStatus: true,
       _id: req.user.id
     }
-    const user = await User.findByIdAndDelete(req.user.id, userCopy, {}).exec();
+    const user = await User.findByIdAndUpdate(req.user.id, userCopy, {}).exec();
     res.render('join', {authenticated: true, result: "You are now a member!! :)"})
   }
 }));
+
+router.get("/message/create", isAuth, expressAsyncHandler(async function(req, res, next) {
+  return res.render("message-form")
+}));
+
+router.post("/message/create", isAuth, [
+  body("title", "The message title must contain a character count of at least 3 and a max of 75")
+  .trim()
+  .isLength({min: 3, max: 75})
+  .escape(),
+  body("text", "The text must contain a character count of at least 3 and a max of 250")
+  .trim()
+  .isLength({min: 3, max: 250})
+  .escape(),
+
+  expressAsyncHandler(async function(req, res, next) {
+    const errors = validationResult(req);
+    const formattedDate = format(new Date(req.body.date), 'MM/dd/yyyy')
+    const message = new Message({
+      title: req.body.title,
+      text: req.body.text,
+      timestamp: formattedDate,
+      author: req.user._id
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('message-form', {
+        message: message,
+        errors: errors.array(),
+      });
+    } else {
+      await message.save();
+      res.redirect('/');
+    }
+  })]
+);
 
 module.exports = router;
