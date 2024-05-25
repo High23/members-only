9db708e207b563211c68e9a2273ff2f8cc24e7bf
High23 <warenasb2@gmail.com>
2024-05-24 22:24:03 -0700

    Adjusted min length

--- models/message.js
+++ models/message.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
 const MessageSchema = new Schema({
-    title: {type: String, minLength: 3, maxLength: 75, required: true},
+    title: {type: String, minLength: 2, maxLength: 75, required: true},
     timestamp: {type: String, required: true},
-    text: {type: String, minLength: 3, maxLength: 250, required: true},
+    text: {type: String, minLength: 2, maxLength: 250, required: true},
     author: {type: Schema.Types.ObjectId, ref: 'User', default: undefined},
 });
 
